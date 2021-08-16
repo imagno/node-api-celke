925438ab7850abc16c40ba0d20f630b6c245df22
Igor Ferreira <igor.magno@protonmail.com>
2021-08-15 21:01:34 -0300

    realizando conexao com o mongodb

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(express.json());
+
+mongoose.connect('mongodb://localhost/imagno', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log("Conexao com o MongoDB realizada com sucesso!")
+}).catch((err) => {
+  console.log("Conexao com o MongoDB NAO realizada com sucesso. :(")
+});
 
 app.get("/", (req, res) => {
   return res.json({titulo: "Como criar API"});
