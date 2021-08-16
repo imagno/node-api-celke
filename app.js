c022d07f950482aa99b0208c4490d02b0823c36f
Igor Ferreira <igor.magno@protonmail.com>
2021-08-15 21:27:26 -0300

    cadastrando no banco de dados

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+require('./models/Artigo');
+const Artigo = mongoose.model('artigo');
+
 const app = express();
 app.use(express.json());
 
@@ -15,6 +18,10 @@ mongoose.connect('mongodb://localhost/imagno', {
 
 app.get("/", (req, res) => {
   return res.json({titulo: "Como criar API"});
+});
+
+app.post("/artigo", (req, res) => {
+  return res.json(req.body);
 });
 
 app.listen(3500, () => {
