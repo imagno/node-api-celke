5c0ed36e719272668ca9f6b3213aa62991c984ff
Igor Ferreira <igor.magno@protonmail.com>
2021-08-15 21:07:16 -0300

    criando models para manipulacao do db

---
+++ models/Artigo.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const Artigo = new mongoose.Schema({
+  titulo: {
+    type: String,
+    required: true
+  },
+  conteudo: {
+    type: String,
+    required: true
+  }
+});
